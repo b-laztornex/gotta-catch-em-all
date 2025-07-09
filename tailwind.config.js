04f834b78fb11cd317f1bfe61186e0d0967ff34e
Marco Casanova <m.casanova.dev@gmail.com>
2025-07-09 08:50:26 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx,js,jsx}",
+    "./node_modules/shadcn/ui/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
