ed35edabcd034c8042346525d8ee9b6b1f4cf8ae
Marco Casanova <m.casanova.dev@gmail.com>
2025-07-09 13:37:00 +0200

    clean Page Client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import PageClient from "@/components/PageClient";
 
 export default async function Home() {
-  const res = await fetch(
-    "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
-  );
-  const { results } = await res.json();
-  return <PageClient data={results} />;
+  return <PageClient />;
 }
